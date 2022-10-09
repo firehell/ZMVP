9ba3b4708a15c4125aebd43b30ff64019937f8c8
zz <13188961863@163.com>
2022-10-09 15:33:12 +0800

    修改zmvp 为android library

--- zmvp/src/androidTest/java/com/zz/zmvp/ExampleInstrumentedTest.kt
+++ zmvp/src/androidTest/java/com/zz/zmvp/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.zz.zmvp", appContext.packageName)
+        assertEquals("com.zz.zmvp.test", appContext.packageName)
     }
 }
